34026fba2692a84bb3ebfc0b5918178e797b76a6
ag1rlisagun <aaliyahwusu@gmail.com>
2024-08-10 14:40:02 -0600

    buttons

--- Scripts/main_elevator_scene.gd
+++ Scripts/main_elevator_scene.gd
@@ -1,8 +1,20 @@
 extends CanvasLayer
 
+# BUTTONS
+@onready var button_num_1 = $UI/NumberButtons/ButtonNumber1
+@onready var button_num_2 = $UI/NumberButtons/ButtonNumber2
+@onready var button_num_3 = $UI/NumberButtons/ButtonNumber3
+@onready var button_num_4 = $UI/NumberButtons/ButtonNumber4
+@onready var button_num_5 = $UI/NumberButtons/ButtonNumber5
+@onready var button_num_6 = $UI/NumberButtons/ButtonNumber6
+@onready var button_num_7 = $UI/NumberButtons/ButtonNumber7
+@onready var button_num_8 = $UI/NumberButtons/ButtonNumber8
+@onready var button_num_9 = $UI/NumberButtons/ButtonNumber9
+@onready var button_num_0 = $UI/NumberButtons/ButtonNumber0
+@onready var help_button = $UI/HelpButton/Help
 
-@onready var button_num_1 = $UI/ButtonNumber1
-
+# SFX
+@onready var SFX_key_press = $SFXPlayers/KeyPress
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +25,37 @@ func _ready():
 func _process(delta):
 	pass
 
+# ELEVATOR BUTTONS
+
 func _on_button_number_1_pressed():
-	Input.action_press("shake")
-	Input.action_release("shake")
-	
-	
+	SFX_key_press.play()
+
+func _on_button_number_2_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_3_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_4_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_5_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_6_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_7_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_8_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_9_pressed():
+	SFX_key_press.play()
+
+func _on_button_number_0_pressed():
+	SFX_key_press.play()
+
+func _on_help_pressed():
+	pass # Replace with function body.
