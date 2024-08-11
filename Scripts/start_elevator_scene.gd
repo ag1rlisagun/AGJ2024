894bf9c118873d89e2877a385ea4f67359bb4591
Moyo Dawodu <moyodawodu@gmail.com>
2024-08-11 09:37:37 -0600

    added delay

--- Scripts/start_elevator_scene.gd
+++ Scripts/start_elevator_scene.gd
@@ -18,6 +18,7 @@ func handle_escape():
 	timer.start()
 
 func _on_start_button_pressed():
+	await get_tree().create_timer(randf_range(3, 10)).timeout
 	SFX_elevator_ding.play()
 	await get_tree().create_timer(1.50).timeout
 	get_tree().change_scene_to_file("res://Scenes/main_elevator_scene.tscn")
